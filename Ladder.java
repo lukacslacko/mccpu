24fa3dc81d7f5c6fd1e2e7e016fe9a0ac811a1c0
lukacslacko <lukacslacko@gmail.com>
2019-03-24 12:41:13 +0100

    Fix ladder slabs

--- Ladder.java
+++ Ladder.java
@@ -21,7 +21,7 @@ public class Ladder implements Renderer {
         for (int i = 0; i < numWires; ++i) {
             target.setTopSlab(bottom.above(2*i));
             target.setWire(bottom.above(2*i+1));
-            target.setTopSlab(bottom.shifted(direction.times(-1)).above(1));
+            target.setTopSlab(bottom.shifted(direction.times(-1)).above(2*i+1));
             target.setWire(bottom.shifted(direction.times(-1)).above(2));
             target.setBlock(bottom.above(2*i).shifted(direction), material);
             target.setRepeater(bottom.above(2*i+1).shifted(direction), Utils.facing(direction));
