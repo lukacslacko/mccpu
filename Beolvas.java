d835afd6e90039b317589654c481a18e1ca6b00a
lukacslacko <lukacslacko@gmail.com>
2019-03-30 20:08:52 +0100

    Beolvas jar

--- Beolvas.java
+++ Beolvas.java
@@ -4,36 +4,36 @@ public class Beolvas{public String[][] olvas(World w,int x,int y,int z,int dx,in
 {for(int ddx = 0; ddx <= dx; ddx++)
 {String s = "";for(int ddz = 0; ddz <= dz; ddz++)
 {Block b = w.getBlockAt(x+ddx,y+ddy,z+ddz);Material m = b.getType();switch(m)
-{case Material.AIR: s += " ";break;
-case Material.REDSTONE_WIRE: s += "+";break;
-case Material.REDSTONE_TORCH: s += "*";break;
-case Material.GLOWSTONE: s += "-";break;
-case Material.QUARZ_SLAB: s += "-";break;
-case Material.REPEATER: Repeater rep = (Repeater)b.getBlockData();switch(rep.getFacing())
-{case BlockFace.NORTH: s += "<";break;
-case BlockFace.EAST: s += "A";break;
-case BlockFace.SOUTH: s += ">";break;
-case BlockFace.WEST: s += "V";break;}
+{case AIR: s += " ";break;
+case REDSTONE_WIRE: s += "+";break;
+case REDSTONE_TORCH: s += "*";break;
+case GLOWSTONE: s += "-";break;
+case QUARTZ_SLAB: s += "-";break;
+case REPEATER: Repeater rep = (Repeater)b.getBlockData();switch(rep.getFacing())
+{case NORTH: s += "<";break;
+case EAST: s += "A";break;
+case SOUTH: s += ">";break;
+case WEST: s += "V";break;}
 break;
-case Material.REDSTONE_WALL_TORCH: RedstoneWallTorch red = (RedstoneWallTorch)b.getBlockData();switch(red.getFacing())
-{case BlockFace.NORTH: s += "]";break;
-case BlockFace.EAST: s += "v";break;
-case BlockFace.SOUTH: s += "[";break;
-case BlockFace.WEST: s += "^";break;}
+case REDSTONE_WALL_TORCH: RedstoneWallTorch red = (RedstoneWallTorch)b.getBlockData();switch(red.getFacing())
+{case NORTH: s += "]";break;
+case EAST: s += "v";break;
+case SOUTH: s += "[";break;
+case WEST: s += "^";break;}
 break;
-case Material.STICKY_PISTON: Piston pis = (Piston)b.getBlockData();switch(pis.getFacing())
-{case BlockFace.NORTH: s += "E";break;
-case BlockFace.EAST: s += "W";break;
-case BlockFace.SOUTH: s += "3";break;
-case BlockFace.WEST: s += "M";break;}
+case STICKY_PISTON: Piston pis = (Piston)b.getBlockData();switch(pis.getFacing())
+{case NORTH: s += "E";break;
+case EAST: s += "W";break;
+case SOUTH: s += "3";break;
+case WEST: s += "M";break;}
 break;
-case Material.COMPARATOR: Comparator com = (Comparator)b.getBlockData();switch(com.getFacing())
-{case BlockFace.NORTH: s += "(";break;
-case BlockFace.EAST: s += "?";break;
-case BlockFace.SOUTH: s += ")";break;
-case BlockFace.WEST: s += "U";break;}
+case COMPARATOR: Comparator com = (Comparator)b.getBlockData();switch(com.getFacing())
+{case NORTH: s += "(";break;
+case EAST: s += "?";break;
+case SOUTH: s += ")";break;
+case WEST: s += "U";break;}
 break;
-case Material.PISTON_HEAD: s += " ";break;
+case PISTON_HEAD: s += " ";break;
 default: s += "X";break;}}
 r[ddy][ddx] = s;}}
 return r;}}                    
