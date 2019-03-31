9f0bc3fc3a1e4f5deca8de6a522742e8bceb8440
lukacslacko <lukacslacko@gmail.com>
2019-03-31 11:25:15 +0200

    jar

--- Pelda.java
+++ Pelda.java
@@ -3,7 +3,7 @@ import org.bukkit.Material;
 public class Pelda extends Component {
     Pelda(Location origin, Vector width, Vector length, Material material) {
         super(origin, width, length, material,
-                new String[] {
+        /*        new String[] {
                         "XXXXXXXXXX",
                         "XXXXXXXXXX",
                         "XXXXXXXXXX",
@@ -29,6 +29,44 @@ public class Pelda extends Component {
                 new String[] {
                         "         ",
                         "         *"
+                });*/
+                new String[] {
+                        " ----------",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                        " XXXXXXXXXX",
+                },
+                new String[] {
+                        "           ",
+                        "      M    ",
+                        "     E*3   ",
+                        "      W    ",
+                        " U?        ",
+                        " () ++<++  ",
+                        "    +   +  ",
+                        " ^  A   V  ",
+                        "]X[ +   +  ",
+                        " v  ++>+++*",
+                },
+                new String[] {
+                        "           ",
+                        "           ",
+                        "           ",
+                        "           ",
+                        "           ",
+                        "           ",
+                        "           ",
+                        "           ",
+                        " *         ",
+                        "           ",
                 });
+
+
     }
 }
