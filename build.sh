ebbb538c2dbb2639af2bd75454f3db5424320eaa
root <root@mc-spigot.europe-west4-a.c.minecraft-server-226616.internal>
2019-03-31 09:55:51 +0000

    Correct local build

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-NAME=mccpu
-FILES=$(ls *.java | sed 's/\..*//' | tr '\n' ',')
-javac $(sed 's/,/.java /g' <<< $FILES) -cp /home/minecraft/spigot-1.13.2.jar
-jar -cf out/$NAME.jar $(sed 's/,/.class /g' <<< $FILES) plugin.yml
+javac *.java -cp /home/minecraft/spigot-1.13.2.jar
+jar -cf out/mccpu.jar *.class plugin.yml
+cp out/mccpu.jar /home/minecraft/plugins/
 rm *.class
-cp out/$NAME.jar /home/minecraft/plugins/
